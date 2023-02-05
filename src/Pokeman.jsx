f8dfc2cb94d1fb45d6dd3afbaef27d7b7c3d7b6a
SimiZeiger <117586967+SimiZeiger@users.noreply.github.com>
2023-02-05 12:28:33 -0500

    got results!

--- src/Pokeman.jsx
+++ src/Pokeman.jsx
@@ -13,11 +13,19 @@ const Pokeman = () => {
 
     // console.log(pokeman);
 
-
+const getPokeman = async () => {
+    try{
+        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon');
+        console.log(data)
+    } catch (err) {
+        console.log(err);
+    }
+};
 
     return (
         <>
             <p>in pokeman component</p>
+            <button onClick={getPokeman}>Get Pokemon</button>
         </>
     ) 
 }
