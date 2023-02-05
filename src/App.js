07fb9dac1e17b0dcc56e163f23394ba898a22f4b
SimiZeiger <117586967+SimiZeiger@users.noreply.github.com>
2023-02-05 12:20:30 -0500

    grabbed pokeman from poki api with try/catch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Pokeman from './Pokeman';
+function App() {
+  return (
+    <>
+      <Pokeman />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pokeman.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const Pokeman = () => {
+//     try{
+//     let pokeman = axios
+//     .get('https://pokeapi.co/api/v2/pokemon')
+//     .then(response => {
+//         console.log(response.data.results);
+//     })
+// } catch (err) {
+//     console.log(err);
+// }
+
+    // console.log(pokeman);
+
+
+
+    return (
+        <>
+            <p>in pokeman component</p>
+        </>
+    ) 
+}
+export default Pokeman
